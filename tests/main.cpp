dc76067bdccaec31aac6791214f992c6e25515eb
Matthew Hoggan <mehoggan@gmail.com>
2015-04-23 23:32:02 -0700

    Test that shows all displays on the same thread equal each other.
    Change-Id: I3a7c981178a5bdc90393655cfc306b8339a42002

--- tests/main.cpp
+++ tests/main.cpp
@@ -26,11 +26,12 @@ int main(int argc, char *argv[])
   assert(d && opengl_core::x11_display::use_count() == 1);
 
   for (unsigned int i = 0; i < 4; ++i) {
-    Display *&d = opengl_core::x11_display::acquire();
-    assert(d && opengl_core::x11_display::use_count() == 2 && (i + 1));
+    Display *&di = opengl_core::x11_display::acquire();
+    assert(di == d && "On a given thread all displays should be the same");
+    assert(di && opengl_core::x11_display::use_count() == 2 && (i + 1));
     opengl_core::x11_display::sync();
     opengl_core::x11_display::release();
-    assert(d && opengl_core::x11_display::use_count() == 1 && (i + 1));
+    assert(di && opengl_core::x11_display::use_count() == 1 && (i + 1));
   }
 
   opengl_core::x11_display::release();
