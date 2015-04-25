702ec60ccf52f311230763401e42af4063b6cb36
Matthew Hoggan <mehoggan@gmail.com>
2015-04-24 19:46:20 -0700

    Changes from work.
    Change-Id: I5acdbd3fbc7666f41681d693396a0352c97a51eb

--- includes/core/x11_display.h
+++ includes/core/x11_display.h
@@ -15,7 +15,7 @@ namespace opengl_core
   private:
     typedef Display X11_Display;
     static thread_local X11_Display *s_display;
-    static thread_local std::atomic<uint32_t> s_use_count;
+    static thread_local uint32_t s_use_count;
 
   public:
     static thread_local X11_Display *&acquire();
@@ -24,7 +24,7 @@ namespace opengl_core
     static thread_local void sync(bool flush_events = true);
     static thread_local void release();
 
-    static thread_local std::atomic<uint32_t> &use_count() {return s_use_count;}
+    static thread_local uint32_t &use_count() {return s_use_count;}
   };
 }
 
