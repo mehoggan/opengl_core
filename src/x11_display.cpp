0c98113b94340dad78af2d5a26b79604961dde95
Matthew Hoggan <mehoggan@gmail.com>
2015-04-24 17:19:45 -0700

    Going with thread local because of instability of x11 cross threaded.

--- src/x11_display.cpp
+++ src/x11_display.cpp
@@ -5,32 +5,17 @@
 namespace opengl_core
 {
   thread_local x11_display::X11_Display *x11_display::s_display = NULL;
-  thread_local std::atomic<uint32_t> x11_display::s_use_count =
-    ATOMIC_VAR_INIT(0u);
+  thread_local uint32_t x11_display::s_use_count = 0u;
 
   x11_display::X11_Display *&x11_display::acquire()
   {
-    uint32_t tmp = 0u;
-    if (std::atomic_compare_exchange_strong(&s_use_count, &tmp, 1u)) {
+    if (s_use_count == 0) {
       s_display = ::XOpenDisplay(NULL); // Open local display only
       assert(s_display && "Failed to open XDisplay");
-    } else {
-      ::XLockDisplay(s_display);
-      std::atomic_fetch_add(&s_use_count, 1u);
-      ::XUnlockDisplay(s_display);
     }
+    ++s_use_count;
 
     return s_display;
-  }
-
-  void x11_display::lock()
-  {
-    ::XLockDisplay(s_display);
-  }
-
-  void x11_display::unlock()
-  {
-    ::XUnlockDisplay(s_display);
   }
 
   void x11_display::sync(bool flush_events)
@@ -40,14 +25,10 @@ namespace opengl_core
 
   void x11_display::release()
   {
-    uint32_t tmp = 1u;
-    if (std::atomic_compare_exchange_strong(&s_use_count, &tmp, 0u)) {
+    if (s_use_count == 1u) {
       ::XCloseDisplay(s_display);
       s_display = 0;
-    } else {
-      ::XLockDisplay(s_display);
-      std::atomic_fetch_sub(&s_use_count, 1u);
-      ::XUnlockDisplay(s_display);
     }
+    --s_use_count;
   }
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -10,8 +10,8 @@ void acquire_display1_hold_m()
 {
   Display *&d = opengl_core::x11_display::acquire();
   assert(d && opengl_core::x11_display::use_count() == 1);
-  std::this_thread::sleep_for(std::chrono::seconds(5));
   opengl_core::x11_display::sync();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   opengl_core::x11_display::release();
   assert(!d && opengl_core::x11_display::use_count() == 0);
 }
@@ -20,27 +20,32 @@ int main(int argc, char *argv[])
 {
   XInitThreads();
 
+  Display *&d = opengl_core::x11_display::acquire();
+  assert(d && opengl_core::x11_display::use_count() == 1);
+
   std::thread test_0(acquire_display1_hold_m);
 
-  Display *&d = opengl_core::x11_display::acquire();
   assert(d && opengl_core::x11_display::use_count() == 1);
 
   for (unsigned int i = 0; i < 4; ++i) {
     Display *&di = opengl_core::x11_display::acquire();
-    assert(di == d && "On a given thread all displays should be the same");
-    assert(di && opengl_core::x11_display::use_count() == 2 && (i + 1));
+    assert(d && opengl_core::x11_display::use_count() == 2);
     opengl_core::x11_display::sync();
     opengl_core::x11_display::release();
-    // Because the display is still alive we can sync after release
     opengl_core::x11_display::sync();
-    assert(di && opengl_core::x11_display::use_count() == 1 && (i + 1));
+    assert(d && opengl_core::x11_display::use_count() == 1);
   }
 
+  assert(d && opengl_core::x11_display::use_count() == 1);
+
   opengl_core::x11_display::release();
+
   assert(!d && opengl_core::x11_display::use_count() == 0);
 
   std::cout << "Waiting on thread to join" << std::endl;
   test_0.join();
+
+  assert(!d && opengl_core::x11_display::use_count() == 0);
 
   return 0;
 }
